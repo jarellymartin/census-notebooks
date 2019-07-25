db6a72d15e839f01d9a6f2e4143c49c524db1101
Yuyang Zhong <yuyang.zhong.2125@gmail.com>
2019-07-24 19:20:21 -0700

    Updates setup & dependencies

--- censusnotebooks/setup.py
+++ censusnotebooks/setup.py
@@ -1,18 +1,25 @@
-import setuptools
+import sys
+from setuptools import setup
+
+if sys.version_info < (3,0):
+    raise ValueError('The censusnotebooks Package requires a Python version greater than 3.0')
+
+with open('requirements.txt') as fid:
+    install_requires = [l.strip() for l in fid.readlines() if l]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="censusnotebooks",
+    packages=['censusnotebooks'],
     version="0.0.1",
-    author="Example Author",
+    author="Sandeep Sainath, Yuyang Zhong",
     author_email="author@example.com",
-    description="A small example package",
+    description="Discovering US Census: Jyputer Notebook Curriculum Modules",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/yuyang-zhong/census-notebooks",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
